893e62598ce93d50c50138ddba41e019e5862d83
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-04-15 17:11:11 +0200

    add comments belongs to user and post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   validates :text, presence: true, length: { minimum: 5 }
   validates :post_id, presence: true
   validates :user_id, presence: true
+  belongs_to: user
+  belongs_to: post
 end
