82486da7e4fa0f9f5ba90f89a17d9771fc318491
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-04-15 17:40:48 +0200

    fix linter error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many: comments
+  has_many :comments
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 10 }
   validates :user_id, presence: true
