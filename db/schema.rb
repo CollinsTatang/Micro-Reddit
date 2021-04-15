f174ea1e06924b48107d43be71eee744bcd588bd
CollinsTatang <makungongcollins@gmail.com>
2021-04-15 14:09:12 +0100

    Edit the schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2021_04_15_092605) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
