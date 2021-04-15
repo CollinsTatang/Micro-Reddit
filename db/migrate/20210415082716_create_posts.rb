fd0ca1e5796cfa679b721b397a0acd766d54ef94
CollinsTatang <makungongcollins@gmail.com>
2021-04-15 14:09:54 +0100

    Remove user id

--- db/migrate/20210415082716_create_posts.rb
+++ db/migrate/20210415082716_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.integer :user_id
       t.timestamps
     end
   end
--- db/migrate/20210415091703_add_user_ref_to_post.rb
+++ db/migrate/20210415091703_add_user_ref_to_post.rb
@@ -1,6 +1,5 @@
 class AddUserRefToPost < ActiveRecord::Migration[6.1]
   def change
-    remove_column(:post, :user_id)
     add_reference :posts, :user, null: false, foreign_key: true
   end
 end
